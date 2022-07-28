f413af04798ca368946114946bf787a4bb57ee97
Octogonapus <firey45@gmail.com>
2022-07-28 16:59:49 -0400

    Restore precompile_execution_file to easily generate new precompiles

--- sysimage/build_sysimage.jl
+++ sysimage/build_sysimage.jl
@@ -6,6 +6,7 @@ function build()
         ["Foo"];
         project = dirname(@__DIR__),
         sysimage_path,
+        # precompile_execution_file = joinpath(@__DIR__, "precompile.jl"),
         precompile_statements_file = joinpath(@__DIR__, "precompile_list.jl"),
         cpu_target = "generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)",
         incremental = true,
