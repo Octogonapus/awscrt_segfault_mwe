1ca7a8145c80a9d6599046aaf744e9c13de25086
Octogonapus <firey45@gmail.com>
2022-07-28 16:51:57 -0400

    Use precompile_statements_file for clarity

--- sysimage/build_sysimage.jl
+++ sysimage/build_sysimage.jl
@@ -6,7 +6,7 @@ function build()
         ["Foo"];
         project = dirname(@__DIR__),
         sysimage_path,
-        precompile_execution_file = joinpath(@__DIR__, "precompile.jl"),
+        precompile_statements_file = joinpath(@__DIR__, "precompile_list.jl"),
         cpu_target = "generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)",
         incremental = true,
         include_transitive_dependencies = false,
